a9e726c92a3d4390acba0d0937a7930eba9e2833
Michael Manlulu <micaheldmanlulu@gmail.com>
2018-08-12 15:06:01 +0800

    - created stocks component

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -1,3 +1,12 @@
 <template>
-    <h1>The Stock/Stock component</h1>
+    <div class="col-sm-6 col-md-4">
+        <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">NAME <small>(Price: PRICE)</small></h5>
+                <input type="number" class="form-control" placeholder="Quantity">
+                <button class="btn btn-success">Buy</button>
+            </div>
+        </div>
+    </div>
 </template>
--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,3 +1,25 @@
 <template>
-    <h1>The Stocks component</h1>
+    <div>
+        <app-stock v-for="stock in stocks" :key="stock.id"></app-stock>
+    </div>
 </template>
+
+<script>
+    import appStock from './Stock.vue';
+
+    export default {
+        data() {
+            return {
+                stocks: [
+                    { id: 1, name: 'Apple', price: 110, imgSrc: '../../assets/images/apple.jpg' },
+                    { id: 2, name: 'Samsung', price: 200, imgSrc: '../../assets/images/samsung.png' },
+                    { id: 3, name: 'Google', price: 250, imgSrc: '../../assets/images/google.jpg' },
+                    { id: 4, name: 'LG', price: 8, imgSrc: '../../assets/images/lg.jpg' }
+                ]
+            }
+        },
+        components: {
+            appStock
+        }
+    }
+</script>
